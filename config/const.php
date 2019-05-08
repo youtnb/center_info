917374f732487501199a284e29da9a005b461eb1
Yusuke Tanabe <y.tanabe@nipponfiling.co.jp>
2019-05-08 11:47:35 +0900

    文言調整

--- config/const.php
+++ config/const.php
@@ -9,12 +9,12 @@ use Cake\Core\Configure;
 
 // 定数の定義
 return [
-    define('TITLE_CENTER','拠点情報管理'),
-    define('TITLE_DEVICE','端末情報管理'),
+    define('TITLE_CENTER', '拠点情報管理'),
+    define('TITLE_DEVICE', '端末情報管理'),
     
-    define('DELETE_CONFIRM','削除してよろしいですか？'),
+    define('DELETE_CONFIRM', '物理削除します。よろしいですか？'),
     
-    define('LIST_CHECK_MARK','ｖ'),
+    define('LIST_CHECK_MARK', 'ｖ'),
     
     define('ROLE_ID_ADMIN', 1),
     define('ROLE_ID_MEMBER', 2),
